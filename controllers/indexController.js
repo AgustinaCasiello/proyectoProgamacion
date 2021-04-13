3903bbacb0cf1d6699f5a1dbf9c4e3fee0f23795
SolBellomo <sbellomo@udesa.edu.ar>
2021-04-13 19:56:38 -0300

    trabajamos con agus pero crashea

---
+++ controllers/indexController.js
@@ -0,0 +1,9 @@
+//const electrodomesticos = require('../products/nuestrosProductos');
+const controller = {
+    login: (req,res) => {
+        res.render('login', {title: 'Login'});
+    },
+};
+ 
+module.exports = controller; 
+
