08c02e8d63065d92e7a2353700fe3a59c4eabc6d
AgustinaCasiello <aguscasiello1@gmail.com>
2021-04-13 23:24:13 -0300

    viendo unos detalles

--- nuestroProyecto/controllers/indexController.js
+++ nuestroProyecto/controllers/indexController.js
@@ -6,4 +6,3 @@ const controller = {
 };
  
 module.exports = controller; 
-
--- nuestroProyecto/routes/login.js
+++ nuestroProyecto/routes/login.js
@@ -1,5 +1,7 @@
 let express = require('express');
 let router = express.Router();
-let controllers = require('../../controllers/indexController');
+let controllers = require('../controllers/indexController');
+
+router.get('/', controllers.login)
 
 module.exports = router; 
