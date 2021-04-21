70e437c8195fa14557f0a4e4ea23e03801048570
Pablo-pujol <ppujol@udesa.edu.ar>
2021-04-21 13:38:57 -0300

    Agregando products

---
+++ nuestroProyecto/products/nuestrosProductos.js
@@ -0,0 +1,40 @@
+const autos = {
+    lista: [
+        {
+            id: 0,
+            nombre: "Audi S3",
+            descripción: "escripción corta del producto Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque delectus ex",
+            image: "/images/autos/840_560.jpg",
+        },
+        {
+            id: 1,
+            nombre: "Audi A3 Sedán",
+            descripción: "Descripción corta del producto Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque delectus ex",
+            image: "/images/autos/A3SEDAN1.jpg",
+        },
+        {
+            id: 2,
+            nombre: "Audi A3 Sportback",
+            descripción: "Descripción corta del producto Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque delectus ex",
+            image: "/images/autos/audi_a3.jpg",
+        },
+        {
+            id: 3,
+            nombre: "",
+            descripción: "Descripción corta del producto Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque delectus ex",
+            image: "",
+        },
+        {
+            id: 4,
+            nombre: "Audi R8 V10",
+            descripción: "Descripción corta del producto Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque delectus ex",
+            image: "/images/autos/r8.jfif",
+        },
+        {
+            id: 5,
+            nombre: "",
+            descripción: "Descripción corta del producto Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque delectus ex",
+            image: "",
+        }
+    ]
+}; 
