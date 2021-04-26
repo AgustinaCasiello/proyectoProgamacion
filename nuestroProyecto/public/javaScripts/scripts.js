61df2df279f4600df8bc9842477417cf53298cb7
SolBellomo <sbellomo@udesa.edu.ar>
2021-04-26 00:08:40 -0300

    boton siguiendo

--- nuestroProyecto/public/javaScripts/scripts.js
+++ nuestroProyecto/public/javaScripts/scripts.js
@@ -1,6 +1,8 @@
+//seguir → siguiendo
 window.onload = function() {
     let siguiendo = document.querySelector('#siguiendo');
     let seguir = document.querySelector('#seguir');
+    
 
     seguir.addEventListener('click', function(){
         siguiendo.style.display = "block";
