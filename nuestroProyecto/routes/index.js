055258812f0d4f1c0915643a4d2483e21adcf28c
SolBellomo <sbellomo@udesa.edu.ar>
2021-05-05 15:04:54 -0300

    proceso de sequelize

--- nuestroProyecto/routes/index.js
+++ nuestroProyecto/routes/index.js
@@ -2,6 +2,8 @@ let express = require('express');
 let router = express.Router();
 let controllers = require('../controllers/indexController');
 
+let db = require('../database/models')
+
 router.get('/', controllers.index)
 
 module.exports = router;
