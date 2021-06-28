2b8d963b45cd558df182019073a1d1f2a1333941
SolBellomo <sbellomo@udesa.edu.ar>
2021-06-28 18:59:21 -0300

    fotos usuarios

--- databaseProyecto/fasten.sql
+++ databaseProyecto/fasten.sql
@@ -42,15 +42,15 @@ FOREIGN KEY (id_producto) REFERENCES productos(id) ON DELETE CASCADE
 );
 
 INSERT INTO usuarios VALUES (DEFAULT,('2021-01-29'),'diego33@gmail.com','Diego33','hola123','dopo.jpg', DEFAULT, DEFAULT);
-INSERT INTO usuarios VALUES (DEFAULT,('2002-03-03'),'Franco@gmail.com','FRancoSantoro','hola123','dopo.jpg', DEFAULT, DEFAULT);
-INSERT INTO usuarios VALUES (DEFAULT,('2001-09-09'),'Anamaria@gmail.com','AnamariaMarcela','hola123','dopo.jpg', DEFAULT, DEFAULT);
-INSERT INTO usuarios VALUES (DEFAULT,('1998-04-23'),'Seba@gmail.com','Sebastian56','hola123','dopo.jpg', DEFAULT, DEFAULT);
-INSERT INTO usuarios VALUES (DEFAULT,('1978-11-17'),'Marcel@gmail.com','MarceloJorge123','hola123','dopo.jpg', DEFAULT, DEFAULT);
-INSERT INTO usuarios VALUES (DEFAULT,('2005-11-14'),'GretaPLanta@gmail.com','Greta Thumberg','hola123','dopo.jpg', DEFAULT, DEFAULT);
-INSERT INTO usuarios VALUES (DEFAULT,('2001-02-02'),'JUan45@gmail.com','Juan Sanchez','hola123','dopo.jpg', DEFAULT, DEFAULT);
-INSERT INTO usuarios VALUES (DEFAULT,('2000-12-12'),'Silvina22@gmail.com','Silvina Zampedri','hola123','dopo.jpg', DEFAULT, DEFAULT);
-INSERT INTO usuarios VALUES (DEFAULT,('2018-12-09'),'Oso08@gmail.com','Lucas Pratto','hola123','dopo.jpg', DEFAULT, DEFAULT);
-INSERT INTO usuarios VALUES (DEFAULT,('1976-09-11'),'juanajason@gmail.com','Juana Jason','hola123','dopo.jpg', DEFAULT, DEFAULT);
+INSERT INTO usuarios VALUES (DEFAULT,('2002-03-03'),'Franco@gmail.com','FRancoSantoro','hola123','foto-1624397935446.jpeg', DEFAULT, DEFAULT);
+INSERT INTO usuarios VALUES (DEFAULT,('2001-09-09'),'Anamaria@gmail.com','AnamariaMarcela','hola123','foto-1624471901658.jpg', DEFAULT, DEFAULT);
+INSERT INTO usuarios VALUES (DEFAULT,('1998-04-23'),'Seba@gmail.com','Sebastian56','hola123','foto-1624733873469.jpg', DEFAULT, DEFAULT);
+INSERT INTO usuarios VALUES (DEFAULT,('1978-11-17'),'Marcel@gmail.com','MarceloJorge123','hola123','DSC_0059.JPG', DEFAULT, DEFAULT);
+INSERT INTO usuarios VALUES (DEFAULT,('2005-11-14'),'GretaPLanta@gmail.com','Greta Thumberg','hola123','DSC_0152.JPG', DEFAULT, DEFAULT);
+INSERT INTO usuarios VALUES (DEFAULT,('2001-02-02'),'JUan45@gmail.com','Juan Sanchez','hola123','gretaThunberg.jpg', DEFAULT, DEFAULT);
+INSERT INTO usuarios VALUES (DEFAULT,('2000-12-12'),'Silvina22@gmail.com','Silvina Zampedri','hola123','luli.jpg', DEFAULT, DEFAULT);
+INSERT INTO usuarios VALUES (DEFAULT,('2018-12-09'),'Oso08@gmail.com','Lucas Pratto','hola123','martu.jpg', DEFAULT, DEFAULT);
+INSERT INTO usuarios VALUES (DEFAULT,('1976-09-11'),'juanajason@gmail.com','Juana Jason','hola123','pedroperez.jpg', DEFAULT, DEFAULT);
 
 INSERT INTO productos VALUES (DEFAULT,'A3SEDAN1.jpg','Audi A3 Sedán',('2021-01-29'),1,'El Audi A3 es un automóvil del segmento C producido por el fabricante alemán Audi desde 1996, abarcando 4 generaciones hasta el momento. Desde finales de verano de 2013 se ofrece también un A3 con carrocería sedán (o Limousine)​. Tiene 4.46 metros de largo, es decir casi 25 cm menos largo que un A4​.', DEFAULT, DEFAULT);
 INSERT INTO productos VALUES (DEFAULT,'840_560.jpg','Audi S3',('2021-01-21'),2,'El motor 2.0 TFSI tiene una potencia de 310 CV y 400 Nm de par. De esta forma, el nuevo Audi S3 acelera de 0 a 100 km/h en solo 4,6 segundos y alcanza la velocidad máxima de 250 km/h. El Nuevo Audi S3 Sportback está equipado con una transmisión S tronic de siete velocidades.', DEFAULT, DEFAULT);
@@ -64,45 +64,45 @@ INSERT INTO productos VALUES (DEFAULT,'mercedesa45.jpg','Mercedes Benz A45 Amg',
 INSERT INTO productos VALUES (DEFAULT,'mercedese63.jpg','Mercedes Benz E63 Amg',('2021-01-20'),10,'Motor naftero V8 4.0 biturbo, con 612 cv de potencia y 850 Nm de torque. Caja automática de nueve velocidades y tracción integral 4Matic+, que incluye distribución completa de torque (incluyendo tracción 100% trasera) y Drifting Mode (ver video abajo). Acelera de 0 a 100 km/h en 3,4 segundos y su velocidad máxima está limitada a 250 km/h.', DEFAULT, DEFAULT);
 
 INSERT INTO comentarios VALUES (DEFAULT,('2021-01-20'),1,1,'Recomendable', 'dopo.jpg', DEFAULT, DEFAULT);
-INSERT INTO comentarios VALUES (DEFAULT,('2021-01-04'),1,1,'Esta bueno','dopo.jpg', DEFAULT, DEFAULT);
-INSERT INTO comentarios VALUES (DEFAULT,('2021-01-20'),1,1,'Prefiero esperar al nuevo modelo','dopo.jpg', DEFAULT, DEFAULT);
-INSERT INTO comentarios VALUES (DEFAULT,('2021-01-03'),1,1,'Ya no lo recomiendo','dopo.jpg', DEFAULT, DEFAULT);
-INSERT INTO comentarios VALUES (DEFAULT,('2021-01-03'),1,2,'No es recomendable','dopo.jpg', DEFAULT, DEFAULT);
-INSERT INTO comentarios VALUES (DEFAULT,('2021-01-13'),3,2,'No me gusto','dopo.jpg', DEFAULT, DEFAULT);
-INSERT INTO comentarios VALUES (DEFAULT,('2021-02-20'),5,2,'Es lindo','dopo.jpg', DEFAULT, DEFAULT);
-INSERT INTO comentarios VALUES (DEFAULT,('2021-01-09'),10,2,'Me parece lento','dopo.jpg', DEFAULT, DEFAULT);
-INSERT INTO comentarios VALUES (DEFAULT,('2021-01-09'),10,3,'Me parece rapido','dopo.jpg', DEFAULT, DEFAULT);
-INSERT INTO comentarios VALUES (DEFAULT,('2021-03-14'),9,3,'Me parece rapido','dopo.jpg', DEFAULT, DEFAULT);
-INSERT INTO comentarios VALUES (DEFAULT,('2021-01-09'),5,3,'Lo quiero cambiar.','dopo.jpg', DEFAULT, DEFAULT);
-INSERT INTO comentarios VALUES (DEFAULT,('2021-01-09'),5,3,'Lo quiero cambiar.','dopo.jpg', DEFAULT, DEFAULT);
-INSERT INTO comentarios VALUES (DEFAULT,('2021-01-09'),7,4,'aMe arrepenti, alto diseño','dopo.jpg', DEFAULT, DEFAULT);
-INSERT INTO comentarios VALUES (DEFAULT,('2021-01-10'),10,4,'Querria saber cuantos años de duracion tiene','dopo.jpg', DEFAULT, DEFAULT);
-INSERT INTO comentarios VALUES (DEFAULT,('2021-04-09'),1,4,'No me termina de convencer','dopo.jpg', DEFAULT, DEFAULT);
-INSERT INTO comentarios VALUES (DEFAULT,('2021-01-15'),7,4,'Buen color','dopo.jpg', DEFAULT, DEFAULT);
-INSERT INTO comentarios VALUES (DEFAULT,('2021-01-12'),10,5,'Parece del futuro','dopo.jpg', DEFAULT, DEFAULT);
-INSERT INTO comentarios VALUES (DEFAULT,('2021-04-11'),1,5,'Esta para ponerlo al sopi','dopo.jpg', DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES (DEFAULT,('2021-01-04'),1,1,'Esta bueno','foto-1624397935446.jpeg', DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES (DEFAULT,('2021-01-20'),1,1,'Prefiero esperar al nuevo modelo','foto-1624471901658.jpg', DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES (DEFAULT,('2021-01-03'),1,1,'Ya no lo recomiendo','foto-1624733873469.jpg', DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES (DEFAULT,('2021-01-03'),1,2,'No es recomendable','pilargonzales.jpg', DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES (DEFAULT,('2021-01-13'),3,2,'No me gusto','pedroperez.jpg', DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES (DEFAULT,('2021-02-20'),5,2,'Es lindo','martu.jpg', DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES (DEFAULT,('2021-01-09'),10,2,'Me parece lento','luli.jpg', DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES (DEFAULT,('2021-01-09'),10,3,'Me parece rapido','gretaThunberg.jpg', DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES (DEFAULT,('2021-03-14'),9,3,'Me parece rapido','foto-1624552694992.JPG', DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES (DEFAULT,('2021-01-09'),5,3,'Lo quiero cambiar.','foto-1624552485995.JPG', DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES (DEFAULT,('2021-01-09'),5,3,'Lo quiero cambiar.','foto-1624545635529.JPG', DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES (DEFAULT,('2021-01-09'),7,4,'aMe arrepenti, alto diseño','foto-1624296847283.JPG', DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES (DEFAULT,('2021-01-10'),10,4,'Querria saber cuantos años de duracion tiene','foto-1624294509981.JPG', DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES (DEFAULT,('2021-04-09'),1,4,'No me termina de convencer','foto-1624299601061.JPG', DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES (DEFAULT,('2021-01-15'),7,4,'Buen color','DSC_0059.JPG', DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES (DEFAULT,('2021-01-12'),10,5,'Parece del futuro','DSC_0152.JPG', DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES (DEFAULT,('2021-04-11'),1,5,'Esta para ponerlo al sopi','DSC_0084.JPG', DEFAULT, DEFAULT);
 INSERT INTO comentarios VALUES (DEFAULT,('2021-04-10'),1,5,'Es horrible','dopo.jpg', DEFAULT, DEFAULT);
-INSERT INTO comentarios VALUES (DEFAULT,('2021-03-29'),2,5,'Quien me lo regala?','dopo.jpg', DEFAULT, DEFAULT);
-INSERT INTO comentarios VALUES (DEFAULT,('2021-02-22'),4,6,'Me parece bastante lindo','dopo.jpg', DEFAULT, DEFAULT);
-INSERT INTO comentarios VALUES (DEFAULT,('2021-04-11'),2,6,'Es electrico?','dopo.jpg', DEFAULT, DEFAULT);
-INSERT INTO comentarios VALUES (DEFAULT,('2021-04-01'),5,6,'El auto de mis sueños','dopo.jpg', DEFAULT, DEFAULT);
-INSERT INTO comentarios VALUES (DEFAULT,('2021-03-29'),1,6,'Parece carisimo. ','dopo.jpg', DEFAULT, DEFAULT);
-INSERT INTO comentarios VALUES (DEFAULT,('2021-02-22'),2,7,'Prefiero un modelo mas viejo.','dopo.jpg', DEFAULT, DEFAULT);
-INSERT INTO comentarios VALUES (DEFAULT,('2021-04-11'),8,7,'No me gusta.','dopo.jpg', DEFAULT, DEFAULT);
-INSERT INTO comentarios VALUES (DEFAULT,('2021-04-01'),10,7,'El auto de mis sueños.','dopo.jpg', DEFAULT, DEFAULT);
-INSERT INTO comentarios VALUES (DEFAULT,('2021-04-23'),3,7,'Es hermoso','dopo.jpg', DEFAULT, DEFAULT);
-INSERT INTO comentarios VALUES (DEFAULT,('2021-01-22'),4,8,'Prefiero un modelo mas viejo.','dopo.jpg', DEFAULT, DEFAULT);
-INSERT INTO comentarios VALUES (DEFAULT,('2021-03-11'),8,8,'No lo compro ni ahi','dopo.jpg', DEFAULT, DEFAULT);
-INSERT INTO comentarios VALUES (DEFAULT,('2021-04-01'),9,8,'Auto feisimo','dopo.jpg', DEFAULT, DEFAULT);
-INSERT INTO comentarios VALUES (DEFAULT,('2021-04-23'),3,8,'Es hermoso','dopo.jpg', DEFAULT, DEFAULT);
-INSERT INTO comentarios VALUES (DEFAULT,('2021-01-22'),4,9,'Ojala alguien me lo regale.','dopo.jpg', DEFAULT, DEFAULT);
-INSERT INTO comentarios VALUES (DEFAULT,('2021-03-11'),8,9,'No se para que veo si no tengo plata','dopo.jpg', DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES (DEFAULT,('2021-03-29'),2,5,'Quien me lo regala?','foto-1624397935446.jpeg', DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES (DEFAULT,('2021-02-22'),4,6,'Me parece bastante lindo','foto-1624471901658.jpg', DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES (DEFAULT,('2021-04-11'),2,6,'Es electrico?','foto-1624733873469.jpg', DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES (DEFAULT,('2021-04-01'),5,6,'El auto de mis sueños','pilargonzales.jpg', DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES (DEFAULT,('2021-03-29'),1,6,'Parece carisimo. ','pedroperez.jpg', DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES (DEFAULT,('2021-02-22'),2,7,'Prefiero un modelo mas viejo.','martu.jpg', DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES (DEFAULT,('2021-04-11'),8,7,'No me gusta.','luli.jpg', DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES (DEFAULT,('2021-04-01'),10,7,'El auto de mis sueños.','gretaThunberg.jpg', DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES (DEFAULT,('2021-04-23'),3,7,'Es hermoso','foto-1624545635529.JPG', DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES (DEFAULT,('2021-01-22'),4,8,'Prefiero un modelo mas viejo.','foto-1624552694992.JPG', DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES (DEFAULT,('2021-03-11'),8,8,'No lo compro ni ahi','foto-1624552485995.JPG', DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES (DEFAULT,('2021-04-01'),9,8,'Auto feisimo','foto-1624294509981.JPG', DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES (DEFAULT,('2021-04-23'),3,8,'Es hermoso','foto-1624299601061.JPG', DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES (DEFAULT,('2021-01-22'),4,9,'Ojala alguien me lo regale.','DSC_0059.JPG', DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES (DEFAULT,('2021-03-11'),8,9,'No se para que veo si no tengo plata','DSC_0152.JPG', DEFAULT, DEFAULT);
 INSERT INTO comentarios VALUES (DEFAULT,('2021-04-01'),9,9,'El color que tiene lo hace ver hermoso','dopo.jpg', DEFAULT, DEFAULT);
-INSERT INTO comentarios VALUES (DEFAULT,('2021-04-02'),3,9,'Viene con 8 velocidades?','dopo.jpg', DEFAULT, DEFAULT);
-INSERT INTO comentarios VALUES (DEFAULT,('2021-01-25'),4,10,'Lo cambiaria por un modelo mas viejo.','dopo.jpg', DEFAULT, DEFAULT);
-INSERT INTO comentarios VALUES (DEFAULT,('2021-02-11'),8,10,'No se para que veo si no tengo plata','dopo.jpg', DEFAULT, DEFAULT);
-INSERT INTO comentarios VALUES (DEFAULT,('2021-03-01'),9,10,'Lo quiero','dopo.jpg', DEFAULT, DEFAULT);
-INSERT INTO comentarios VALUES (DEFAULT,('2021-04-11'),2,10,'Es electrico?','dopo.jpg', DEFAULT, DEFAULT); -- DEFAULT, DEFAULT para que se cree el createdAt y updatedAt
+INSERT INTO comentarios VALUES (DEFAULT,('2021-04-02'),3,9,'Viene con 8 velocidades?','foto-1624397935446.jpeg', DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES (DEFAULT,('2021-01-25'),4,10,'Lo cambiaria por un modelo mas viejo.','gretaThunberg.jpg', DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES (DEFAULT,('2021-02-11'),8,10,'No se para que veo si no tengo plata','DSC_0084.JPG', DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES (DEFAULT,('2021-03-01'),9,10,'Lo quiero','DSC_0084.JPG', DEFAULT, DEFAULT);
+INSERT INTO comentarios VALUES (DEFAULT,('2021-04-11'),2,10,'Es electrico?','foto-1624299601061.JPG', DEFAULT, DEFAULT); -- DEFAULT, DEFAULT para que se cree el createdAt y updatedAt
 
 
 
